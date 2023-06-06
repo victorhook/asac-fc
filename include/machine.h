001b00c07f9cdc7c43556fd7d00410ef903619a9
victor <victorkrook96@gmail.com>
2023-06-06 21:37:22 +0200

    Created new revision for PCB: - Switched to USB-C - Switched IMU to BMI270 - Removed connectors on bottom side for ESC - Switched to 4 layer board
    Ordered it, this time with PCB assembly on top side,
    since I had big issues with soldering the IMU by hand
    last time. Let's hope this rev goes better!

--- include/machine.h
+++ include/machine.h
@@ -2,34 +2,33 @@
 #define MACHINE_H
 
 /*
-Pin definitions for custom PCB
-#define PIN_M1            3
-#define PIN_M2            0
-#define PIN_M3            25
-#define PIN_M4            17
+Pin definitions for custom PCB - Revision B
+#define PIN_M1                 1
+#define PIN_M2                 0
+#define PIN_M3                20
+#define PIN_M4                18
 
-#define PIN_BUZZER        11
-#define PIN_CURR          1
-#define PIN_VBAT_ADC      27
-#define VBAT_ADC_INPUT_NUMBER   1
-#define PIN_IMU_INTERRUPT 2
-#define PIN_RX_ESC        29
+#define PIN_VBAT_ADC          28
+#define VBAT_ADC_INPUT_NUMBER  2
 
-#define IMU_I2C_NUMBER 0
-#define PIN_SDA0          4
-#define PIN_SCL0          5
-#define PIN_SDA1          6
-#define PIN_SCL1          7
+#define PIN_MOSI              11
+#define PIN_MISO              12
+#define PIN_CS                13
+#define PIN_SCK               14
+#define PIN_IMU_INTERRUPT_1   10
+#define PIN_IMU_INTERRUPT_2    9
 
-#define PIN_TX1           8
-#define PIN_RX1           9
-#define PIN_TX0           12
-#define PIN_RX0           13
+#define PIN_SDA1               2
+#define PIN_SCL1               3
 
-#define PIN_LED           10
-#define PIN_LED_BLUE      18
-#define PIN_LED_GREEN     19
-#define PIN_LED_RED       26
+#define PIN_TX1                4
+#define PIN_RX1                5
+#define PIN_TX2_PIO            6
+#define PIN_RX2_PIO            7
+
+#define PIN_LED_BLUE           8
+#define PIN_LED_GREEN          27
+#define PIN_LED_RED            26
 */
 
 #define ASAC_FC_FLASH_SIZE        (2 * 1024 * 1024)
