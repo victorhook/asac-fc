a8d874abecd07206b9a2c56e5fdcf9ca199b5b4e
victor <victorkrook96@gmail.com>
2023-08-27 11:50:13 +0200

    Changed default PID settings

--- src/mavlink_params.c
+++ src/mavlink_params.c
@@ -3,67 +3,68 @@
 
 system_params_t system_params = {
     .pid_gyro_roll_p = {
-        .param_value = 99,
+        .param_value = 1,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_roll_p",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_roll_i = {
-        .param_value = 99,
+        .param_value = 0.75,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_roll_i",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_roll_d = {
-        .param_value = 99,
+        .param_value = 0.001,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_roll_d",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_pitch_p = {
-        .param_value = 99,
+        .param_value = 1,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_pitch_p",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_pitch_i = {
-        .param_value = 99,
+        .param_value = 0.75,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_pitch_i",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_pitch_d = {
-        .param_value = 99,
+        .param_value = 0.001,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_pitch_d",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_yaw_p = {
-        .param_value = 99,
+        .param_value = 0.5,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_yaw_p",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_yaw_i = {
-        .param_value = 99,
+        .param_value = 0.35,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_yaw_i",
         .param_type = MAV_PARAM_TYPE_REAL32
     },
     .pid_gyro_yaw_d = {
-        .param_value = 99,
+        .param_value = 0,
         .param_count = 0,
         .param_index = 0,
         .param_id = "pid_gyro_yaw_d",
         .param_type = MAV_PARAM_TYPE_REAL32
+    },
     }
 };
 
