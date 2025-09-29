9922512622021910879452648945e271f0addb69
Victor <victorkrook96@gmail.com>
2025-09-29 21:19:44 +0200

    led cleanup

--- src/led/led.c
+++ src/led/led.c
@@ -1,5 +1,7 @@
 #include "led.h"
 #include "hal.h"
+#include "mavlink.h"
+#include "mavlink_driver/mavlink_driver.h"
 
 #define GREEN_OFF_RED_OFF() {gpio_put(PIN_LED_GREEN, 0); gpio_put(PIN_LED_RED, 0);}
 #define GREEN_ON_RED_OFF()  {gpio_put(PIN_LED_GREEN, 1); gpio_put(PIN_LED_RED, 0);}
@@ -7,25 +9,33 @@
 #define GREEN_ON_RED_ON()   {gpio_put(PIN_LED_GREEN, 1); gpio_put(PIN_LED_RED, 1);}
 
 
-int led_init() {/*
-    gpio_init(PIN_LED_GREEN);
-    gpio_init(PIN_LED_RED);
-    gpio_init(PIN_LED_BLUE);
-    gpio_set_dir(PIN_LED_GREEN, GPIO_OUT);
-    gpio_set_dir(PIN_LED_RED, GPIO_OUT);
-    gpio_set_dir(PIN_LED_BLUE, GPIO_OUT);
-    gpio_put(PIN_LED_GREEN, 0);
-    gpio_put(PIN_LED_RED, 0);
-    gpio_put(PIN_LED_BLUE, 0);
-*/
+int led_init()
+{
+    brd_led1 = 19;
+    brd_led2 = 20;
+    hal_gpio_init(brd_led1, HAL_GPIO_FUNCTION_OUTPUT, 0);
+    hal_gpio_init(brd_led2, HAL_GPIO_FUNCTION_OUTPUT, 0);
+    hal_gpio_init(brd_led3, HAL_GPIO_FUNCTION_OUTPUT, 0);
+
+
     return 0;
 }
 
-void led_set(uint8_t pin, uint8_t value) {
-    /*
-    gpio_put(pin, value);*/
+
+void led_on(const uint8_t pin)
+{
+    hal_gpio_set(pin, 1);
 }
 
+void led_off(const uint8_t pin)
+{
+    hal_gpio_set(pin, 0);
+}
+
+void led_blink(const uint8_t pin, const uint16_t on_time, const uint16_t off_time)
+{
+
+}
 
 void led_run_boot_sequence() {
     /*
--- src/led/led.h
+++ src/led/led.h
@@ -3,18 +3,27 @@
 
 #include "stdint.h"
 
-#define LED_RED   0 // PIN_LED_RED
-#define LED_GREEN 0 // PIN_LED_GREEN
-#define LED_BLUE  0 // PIN_LED_BLUE
-
+extern float brd_led1;
+extern float brd_led2;
+extern float brd_led3;
 
 int led_init();
 
+void led_on(const uint8_t pin);
 
-void led_set(uint8_t pin, uint8_t value);
+void led_off(const uint8_t pin);
 
+void led_blink(const uint8_t pin, const uint16_t on_time, const uint16_t off_time);
 
 void led_run_boot_sequence();
 
+static inline void led1_on() { led_on(brd_led1); }
+static inline void led1_off() { led_off(brd_led1); }
+
+static inline void led2_on() { led_on(brd_led2); }
+static inline void led2_off() { led_off(brd_led2); }
+
+static inline void led3_on() { led_on(brd_led3); }
+static inline void led3_off() { led_off(brd_led3); }
 
 #endif /* LED_H */
