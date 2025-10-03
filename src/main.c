44bf8b0e30e61bab15de91d114b50d3c4356d4e8
Victor <victorkrook96@gmail.com>
2025-10-03 06:38:38 +0200

    Main: Cleanup and refactor

--- src/main.c
+++ src/main.c
@@ -1,26 +1,47 @@
+#include "hal.h"
+#include "hal/hal.h"
 #include "mavlink.h"
 #include "util.h"
 #include "motor/motor.h"
-#include "rc/receiver.h"
+#include "ahrs/ahrs.h"
+#include "rc/rc.h"
 #include "imu/imu.h"
 #include "control/controller.h"
 #include "led/led.h"
-#include "battery/battery_adc.h"
+#include "battery/battery.h"
 #include "param/param.h"
 #include "state.h"
 #include "mavlink_driver/mavlink_driver.h"
-#include "serial/serial.h"
+#include "serial.h"
+#include "control/controller.h"
+
+#include "scheduler.h"
+#include "util/lpf.h"
 
 static void init_driver(int (*init_function)(), const char* name);
 
+static int driver = 0;
 static int init_result = 0;
+
+
+task_t tasks[] =
+{
+    {.update = controller_pid_loop, .name = "PID", .loop_divider = 1},
+    {.update = ahrs_update, .name = "AHRS", .loop_divider = 1},
+    {.update = mavlink_driver_update, .name = "MAVLink", .loop_divider = 1},
+    {.update = hal_serial_update, .name = "Serials", .loop_divider = 1},
+    {.update = rc_update, .name = "RC", .loop_divider = 2},
+    {.update = battery_update, .name = "Battery", .loop_divider = 10}
+};
 
 int main()
 {
+    // Initialize HAL - Serial, SPI, I2C etc. This also includes buffer initialization for the buses
     hal_init();
+    
+    //read_parameters();
 
     // Initialize USB serial to allow messages to be sent/buffered to GCS
-    serial_usb_init();
     mavlink_driver_init();
 
     // Initialize all drivers
@@ -30,59 +51,30 @@ int main()
     // Initialize LED driver and blink boot-up sequence
     init_driver(led_init, "Led");
     led_run_boot_sequence();
-    led_set(LED_RED, 1);
+    led1_on();
 
-    //read_parameters();
-
-    init_driver(receiver_init,       "Receiver");
-    init_driver(battery_adc_init,    "Battery ADC");
+    // Initialize rest of drivers
+    init_driver(battery_init,    "Battery");
     init_driver(imu_init,            "IMU");
     init_driver(controller_init,     "Controller");
     init_driver(motors_init,         "Motors");
+    init_driver(rc_init,             "Receiver");
+    init_driver(ahrs_init,             "AHRS");
+
+    // At last we'll initialize the controller
+    init_driver(controller_init,         "Controller");
+
+    // Initialize scheduler
+    scheduler_init(tasks, sizeof(tasks) / sizeof(task_t));
 
     // Done booting
-    led_set(LED_RED, 0);
-    led_set(LED_GREEN, 1);
+    led1_off();
+    led2_on();
     state.mode = MODE_IDLE;
-
     gcs_printf(MAV_SEVERITY_INFO, "Drivers initialized, let's go!");
 
-    while (1)
-    {
-        const uint32_t period_us = 1000;
-        uint32_t next_loop = hal_micros();
-        static uint64_t frame = 0;
-
-        while (1)
-        {
-            if (frame % 1000 == 0)   // 1 Hz
-            {
-
-            }
-            if (frame % 100 == 0)    // 10 Hz
-            {
-
-            }
-            if (frame % 10 == 0)      // 100 Hz
-            {
-            }
-            mavlink_driver_update();
-
-            // 1 kHz
-            //controller_pid_loop();
-
-            // Wait for next loop
-            int time_to_sleep = next_loop - hal_micros();
-            
-            if (time_to_sleep > 0)
-            {
-                hal_sleep_us(time_to_sleep);
-            }
-
-            next_loop += period_us;
-            frame++;
-        }
-    }
+    // Start scheduler, this will never return, let's go!
+    scheduler_run();
 
     return 0;
 }
@@ -92,11 +84,15 @@ int main()
 static void init_driver(int (*init_function)(), const char* name)
 {
     int res = init_function();
+
     if (res == 0) {
-        gcs_printf(MAV_SEVERITY_DEBUG, "Init %s OK", name);
+        gcs_printf(MAV_SEVERITY_DEBUG, "Init [OK] %s", name);
     } else {
-        gcs_printf(MAV_SEVERITY_ERROR, "Init %s: Error (%d)", name, res);
+        gcs_printf(MAV_SEVERITY_ERROR, "Init [ER] %s: (%d)", name, res);
     }
 
-    init_result |= res;
+    driver++;
+
+    // Update initresult bitmask
+    init_result |= (res << driver);
 }
