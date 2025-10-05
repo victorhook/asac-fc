cc0a1fe79e4bf3d916506e5a24e257a732ced2d7
Victor <victorkrook96@gmail.com>
2025-10-05 17:14:14 +0200

    State: Added some states & moved flightmode_t into state.h

--- src/state.c
+++ src/state.c
@@ -2,12 +2,17 @@
 
 
 state_t state = {
-    .is_armed         = false,
-    .is_force_armed   = false,
-    .is_rc_connected  = false,
-    .is_usb_connected = false,
+    .armed         = false,
+    .force_armed   = false,
+    .rc_connected  = false,
+    .usb_connected = false,
+    .imu_is_working   = false,
     .run_motor_test   = false,
     .mode             = MODE_BOOTING,
+    .flightmode       = FLIGHTMODE_ACRO,
+    .bat_volt_mv      = 0,
+    .bat_curr_ma      = -1,
+    .bat_remain       = -1,
     .roll             = 0,
     .pitch            = 0,
     .yaw              = 0,
@@ -22,5 +27,13 @@ state_t state = {
 
 bool allow_rebooting()
 {
-    return !state.is_armed && !state.is_force_armed;
+    return !state.armed && !state.force_armed;
+}
+
+int arming_check()
+{
+    return
+        (state.rc_connected)   &&
+        (state.imu_is_working) &&
+        (!state.usb_connected);
 }
--- src/state.h
+++ src/state.h
@@ -3,6 +3,12 @@
 
 #include "util.h"
 
+typedef enum
+{
+    FLIGHTMODE_STABILIZE = 0,
+    FLIGHTMODE_ACRO = 1
+} flightmode_t;
+
 
 typedef enum {
     MODE_BOOTING,
@@ -12,13 +18,19 @@ typedef enum {
 } drone_mode_t;
 
 typedef struct {
-    bool is_armed;
-    bool is_force_armed;
-    bool is_rc_connected;
-    bool is_usb_connected;
+    bool armed;
+    bool force_armed;
+    bool rc_connected;
+    bool usb_connected;
     bool run_motor_test;    
+    bool imu_is_working;
 
     drone_mode_t mode;
+    flightmode_t flightmode;
+
+    uint16_t bat_volt_mv;
+    int16_t bat_curr_ma;
+    int8_t bat_remain;
 
     float        roll;
     float        pitch;
@@ -40,6 +52,8 @@ typedef struct {
 extern state_t state;
 
 bool allow_rebooting();
+
+int arming_check();
 
 
 #endif /* STATE_H */
