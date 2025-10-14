fb61f4cc838818194a4bf546cdfa524e81203efe
Victor <victorkrook96@gmail.com>
2025-10-14 06:25:46 +0200

    IMU: Dummy sitl values for imu reading

--- src/imu/imu_sitl.c
+++ src/imu/imu_sitl.c
@@ -7,5 +7,11 @@ int imu_sitl_do_init(imu_backend_bus_t* bus)
 
 bool imu_sitl_do_read(imu_reading_t* imu_reading)
 {
+    imu_reading->acc_x = 0;
+    imu_reading->acc_y = 0;
+    imu_reading->acc_z = 9.82;
+    imu_reading->gyro_x = 0;
+    imu_reading->gyro_y = 0;
+    imu_reading->gyro_z = 0;
     return true;
 }
